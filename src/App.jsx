a9da1106fefb452297bda298e3c8846c0e9ac89a
HeberRios <68972507+HeberRios@users.noreply.github.com>
2024-12-02 14:17:24 -0600

    Add the header of the movie finder App

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <p>Hey yo!</p>
-    </>
+    <div className='page'>
+      <header>
+        <h1>Movie Finder App</h1>
+        <form className='movies-form'>
+          <input
+            type='text'
+            name='query'
+            id='query'
+            placeholder='Avengers, Parasite, Django ...'
+            className='movie-query-input'
+          />
+          <button type='submit'>Search</button>
+        </form>
+      </header>
+    </div>
   );
 }
 
