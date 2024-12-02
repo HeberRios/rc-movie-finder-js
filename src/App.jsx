859c37345ba40d3893baead475ff894b6bb14ec7
HeberRios <68972507+HeberRios@users.noreply.github.com>
2024-12-02 15:31:08 -0600

    Show movies with data scraped from the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import queryResults from './mocks/query-with-results.json';
 
 function App() {
+  const movies = queryResults.Search;
+
   return (
     <div className='page'>
-      <header>
+      <header className='container'>
         <h1>Movie Finder App</h1>
         <form className='movies-form'>
           <input
@@ -16,6 +19,25 @@ function App() {
           <button type='submit'>Search</button>
         </form>
       </header>
+
+      <main className='container'>
+        <h2>Results</h2>
+
+        <ul className='query-results-container'>
+          {movies.length > 0 &&
+            movies.map(function (movie) {
+              return (
+                <li className='movie' key={movie.imdbID}>
+                  <h3>{movie.Title}</h3>
+                  <span>
+                    <time dateTime={movie.Year}>{movie.Year}</time>
+                  </span>
+                  <img src={movie.Poster} alt={movie.Title} />
+                </li>
+              );
+            })}
+        </ul>
+      </main>
     </div>
   );
 }
